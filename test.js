1246119468d213fd42b29866b6a5471c91437853
halim jung <kyozzang20@gmail.com>
2018-12-07 13:18:28 +0900

    CRLF -> LF

--- test.js
+++ test.js
@@ -10,9 +10,10 @@ const parser = require('./parser');
 //   },
 // };
 
-const result = parser.parse('http://blog.daum.net/mc529/1410');
+parser.parse('http://blog.daum.net/mc529/1410').then((result) => {
+  console.log(result);
+});
 // const result2 = parser.parse('https://medium.com/s/story/the-monstrous-immorality-of-creating-genetically-engineered-babies-6c7e409c9490');
 
-console.log(result);
 // console.log(result.title);
 // console.log(result.content);
