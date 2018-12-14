5ec5060d88dd8994261b57cf0d4f9672645bebad
halim.j <kyozzang20@gmail.com>
2018-12-14 18:22:54 +0900

    medium promise화 완료

--- parser_module/mediumParser.js
+++ parser_module/mediumParser.js
@@ -1,17 +1,21 @@
 const cheerio = require('cheerio');
-const rp = require('request-promise-native');
+const rpn = require('request-promise-native');
 
 exports.parse = (url) => {
-  const headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36',
-    'Content-Type': 'application/x-www-form-urlencoded',
-  };
-  rp.get({ url; url.href, headers }, (html) => {
-    const $ = cheerio.load(html);
+  let serverData;
 
-    return {
-      title: $('title').text(),
-      content: $('.sectionLayout--insetColumn').text(),
-    };
+  const options = {
+    uri: url.href,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  };
+  
+  return rpn.get(options).then((html) => {
+	  const $ = cheerio.load(html);
+	  const title = $('title').text();
+	  const content = $('.sectionLayout--insetColumn').text();
+	  return { title, content };  
   });
 };
--- test.js
+++ test.js
@@ -10,8 +10,8 @@ const parser = require('./parser');
 //   },
 // };
 
-const result = parser.parse('https://medium.com/@minsoogwak/i-am-a-full-stack-developer-836119359410')
-console.log(result);
+parser.parse('https://medium.com/@minsoogwak/i-am-a-full-stack-developer-836119359410').then((value) => {console.log(value)});
+// const result = parser.parse('https://blog.naver.com/pjh731/221419121474');
 // const result2 = parser.parse('https://medium.com/s/story/the-monstrous-immorality-of-creating-genetically-engineered-babies-6c7e409c9490');
 // console.log(result.title);
 // console.log(result.content);
