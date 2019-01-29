55edd1adc14db3ba209f1a85eabb35679681d0ae
halim.j <kyozzang20@gmail.com>
2019-01-29 18:04:08 +0900

    type 상수 추가

--- parser.js
+++ parser.js
@@ -6,28 +6,42 @@ const commonParser = require('./parser_module/commonParser');
 const mediumParser = require('./parser_module/mediumParser');
 const bloggerParser = require('./parser_module/bloggerParser');
 
+const TYPE_NAVER = 0;
+const TYPE_TISTORY = 1;
+const TYPE_DAUM = 2;
+const TYPE_MEDIUM = 3;
+const TYPE_BLOGGER = 4;
+const TYPE_WORDPRESS = 5;
+
+module.exports = {
+  TYPE_NAVER,
+  TYPE_TISTORY,
+  TYPE_DAUM,
+  TYPE_MEDIUM,
+  TYPE_BLOGGER,
+  TYPE_WORDPRESS,
+};
 
 module.exports = {
   parse: (_url, type, TagOptions) => {
     let templateObj;
     const parsedObj = url.parse(_url);
-    if (parsedObj.host.includes('naver') || type === 'naver') {
+    if (parsedObj.host.includes('naver') || type === TYPE_NAVER) {
       templateObj = naverParser.parse(parsedObj);
-    } else if (parsedObj.host.includes('tistory') || type === 'tistory') {
+    } else if (parsedObj.host.includes('tistory') || type === TYPE_TISTORY) {
       templateObj = tistoryParser.parse(parsedObj);
-    } else if (parsedObj.host.includes('daum') || type === 'daum') {
+    } else if (parsedObj.host.includes('daum') || type === TYPE_DAUM) {
       templateObj = daumParser.parse(parsedObj);
-    } else if (parsedObj.host.includes('medium') || type === 'medium') {
+    } else if (parsedObj.host.includes('medium') || type === TYPE_MEDIUM) {
       templateObj = mediumParser.parse(parsedObj);
     } else if (parsedObj.host.includes('blogger') || parsedObj.host.includes('blogspot')
-    || type === 'blogger') {
+    || type === TYPE_BLOGGER) {
       templateObj = bloggerParser.parse(parsedObj);
-    } else if (type === 'wordpress') {
+    } else if (type === TYPE_WORDPRESS) {
       templateObj = commonParser.parse(parsedObj, TagOptions);
     } else {
       templateObj = commonParser.parse(parsedObj, TagOptions);
     }
-
     return templateObj;
   },
 };
